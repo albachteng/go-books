236ccb6f490a885672d79531e9adc8f2c9fcb83c
Albachten <albachtg@LEGAL.REGN.NET>
2022-04-06 20:22:23 -0500

    add model and config gorm

---
+++ pkg/config/app.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	// nts why _ ?
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	// NTS replace second param with sql connection
+	d, err := gorm.Open("mysql", "akhil:Axlesharma@12@/simplerest?charset=utf8&parseTime=True*loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
